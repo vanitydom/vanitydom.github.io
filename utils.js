1e25210a7afbb6e600e4ac1f4c379dfb2e1b46de
vanitydom <137230544+vanitydom@users.noreply.github.com>
2023-06-20 20:43:47 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const cp = require('child_process');
 
 function lastUpdate(glob='[0-9]*.json') {
   //const command = `git log -1 "--date=format: %B %d, %Y" --format=%cd ${glob}`
-  const command = `git log -1 "--date=format: %A %B %d, %Y" --format=%cd ${glob}`
+  const command = `git log -1 "--date=format: %A %B %d, %Y" --format=%ad ${glob}`
   return cp.execSync(command).toString().trim()
 }
 
